b42f4516255c4d67e4352784a38eefd638a625de
iamalexdev <104172767+iamalexdev@users.noreply.github.com>
2025-10-17 20:20:21 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("7630853977:AAGrnl9XdzC-8eONDIp-8NM-uqimlYboFcc)
+API_FOOTBALL_KEY = os.getenv("aa0d5dc769cfaf7d2ad217ef96db276e")
 
 if not TELEGRAM_BOT_TOKEN or not API_FOOTBALL_KEY:
     raise ValueError("⚠️ Debes configurar TELEGRAM_BOT_TOKEN y API_FOOTBALL_KEY en las variables de entorno.")
+
